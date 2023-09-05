5008ec762df9ec1f9125984f1302176e9f2b8682
noeldulin <noel.dulin@august99.com>
2023-09-05 09:00:28 +0800

    Added slack notification

---
+++ slack-notif-start.sh
@@ -0,0 +1,11 @@
+WEBHOOK="https://hooks.slack.com/services/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" 
+
+curl -X POST \
+-H 'Content-type: application/json' \
+--data '{ "channel": "#automation-notifs", 
+            "username": "Realph Automation", 
+            "icon_url": "https://real.ph/public/front/lib/images/favicon.png", 
+            "text": "*'$CI_JOB_NAME'* has been started", 
+            "color": "warning", 
+            "pretext": "Automation Test Status:" }' \
+"$WEBHOOK"
