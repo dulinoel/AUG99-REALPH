032031d61235e7eabdc219e66a0e2ea73963e0f7
noeldulin <noel.dulin@august99.com>
2023-09-06 18:39:34 +0800

    Added logs on slack failed notification

--- slack-notif-failed.sh
+++ slack-notif-failed.sh
@@ -5,7 +5,7 @@ curl -X POST \
 --data '{ "channel": "#automation-notifs", 
             "username": "Realph Automation", 
             "icon_url": "https://real.ph/public/front/lib/images/favicon.png", 
-            "text": "*'$CI_JOB_NAME'* has failed", 
+            "text": "*'$CI_JOB_NAME'* has failed \n check logs here: *'$CI_JOB_URL'*", 
             "color": "danger", 
             "pretext": "Automation Test Status:" }' \
 "$WEBHOOK"
